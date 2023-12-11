f49061a4137e8f4400db7c7b46a3670447598ddd
Povarnitsina Sonya <73982948+povar0305@users.noreply.github.com>
2023-12-11 11:43:45 +0300

    input

--- src/components/input/npk-input.stories.ts
+++ src/components/input/npk-input.stories.ts
@@ -7,39 +7,28 @@ export default {
         modelValue: {type:'string'},
         label: {type:'string'},
         status: {type:'string'},
-        // disabled: {
-        //     control: { type: 'boolean' },
-        //     defaultValue: false,
-        // },
-        // size: {
-        //     control: { type: 'radio' },
-        //     options: ['small', 'medium', 'large'],
-        //     defaultValue: 'medium',
-        // },
-        // color: {
-        //     control: { type: 'select' },
-        //     options: ['primary', 'secondary'],
-        //     defaultValue: 'primary',
-        // },
+        placeholder: {type:'string'},
+        type:{type:'string'}
     },
 } as Meta<typeof Component>;
-
-const Template: StoryFn<typeof Component> = (args) => ({
-    components: { Component },
-    setup() {
-        return { args };
-    },
-    template: `
-      <Component v-bind="args" />
-  `,
-});
 
 export const Default: StoryFn<typeof Component> = (args) => ({
     components: { Component },
     setup() {
         return { args };
     },
-    template: '{{args}}<Component v-bind="args" />',
+
+    template: '<Component />',
+
+});
+
+
+export const Search: StoryFn<typeof Component> = (args) => ({
+    components: { Component },
+    setup() {
+        return { args };
+    },
+    template: '<Component  type="search"/>',
 });
 //
 // export const PrimaryButton = Template.bind({});
--- src/components/input/npk-input.vue
+++ src/components/input/npk-input.vue
@@ -3,14 +3,20 @@ const properties=defineProps({
   modelValue: { type: String, required: true},
   label: { type: String, required: false },
   status: { type: String, required: false },
+  placeholder: { type: String, required: false },
+  type: { type: String, required: false },
 })
+console.log('npk-input',properties)
 </script>
 
 <template>
 <div class="wrapper">
-  <span class="label">{{label}}</span>
-  <input type="text" :value="modelValue"/>
-  <span class="status">{{ status }}</span>
+  <span class="label" v-if="label">{{label}}</span>
+  <div class="input">
+    <input type="text" :value="modelValue" :placeholder="placeholder"/>
+    {{type}}
+  </div>
+  <span class="status" v-show="status">{{ status }}</span>
 </div>
 </template>
 
